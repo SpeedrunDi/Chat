776c659aae3ef0b0f1d58a5c2e1dc313bc9c5450
Tolgonai Andakulova <tolgonaia@gmail.com>
2022-10-15 22:39:11 +0600

    CP-3 added route for messages

--- api/index.js
+++ api/index.js
@@ -13,8 +13,66 @@ app.use(cors());
 const port = 8000;
 
 const users = require('./app/users');
+const User = require("./models/User");
 
 app.use('/users', users);
+
+const onlineConnections = {};
+
+app.ws('/messages', async(ws, req) => {
+  const token = req.query.token;
+
+  if(!token) {
+    ws.send(JSON.stringify({
+    error: 'Wrong token'
+    }))
+  }
+
+  const user = await User.findOne({token: token});
+
+  if(!user) {
+    ws.send(JSON.stringify({
+      error: 'User not found'
+    }));
+  }
+
+  console.log('Client connected id = ', user._id);
+  onlineConnections[user._id] = ws;
+
+  ws.send(JSON.stringify({
+    type: 'CONNECTED',
+    user
+  }));
+
+  ws.on('close', () => {
+    console.log('Client disconnected id = ', user._id);
+    delete onlineConnections[user._id]
+  });
+
+  ws.on('message', msg => {
+    const newMessage = JSON.parse(msg);
+
+    switch(newMessage.type) {
+      case 'POST_MESSAGE':
+        Object.keys(onlineConnections).forEach(connectId => {
+          const connect = onlineConnections[connectId];
+
+          connect.send(JSON.stringify(({
+            type: 'NEW_MESSAGE',
+            message: {
+              user,
+              text: newMessage.message
+            }
+          })))
+        });
+        break;
+      default:
+        console.log('Unknown message type: ', newMessage.type)
+    }
+    ws.send(msg)
+  })
+  console.log(onlineConnections);
+})
 
 const run = async () => {
   await mongoose.connect(config.db.url, config.db.options);
