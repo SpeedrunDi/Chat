4f4e7c7290fe8bfc127c2be58049e87ce6899895
Ilim Alybekov <ilimalybekov2204@gmail.com>
2022-10-16 19:21:41 +0600

    CP-4 username saved in localStorage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Router} from 'react-router-dom';
-import history from './history'
-import App from './App';
 import {Provider} from "react-redux";
-import store from "./store/configureStore";
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import thunk from "redux-thunk";
+
+import history from './history'
+import usersReducer from "./store/reducers/usersReducer";
+import App from './App';
+
+const saveToLocalStorage = state => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState);
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+const loadFromLocalStorage = () => {
+    try {
+        const serializedState = localStorage.getItem('state');
+
+        if (serializedState === null) return undefined;
+
+        return JSON.parse(serializedState);
+    } catch (e) {
+        console.error(e);
+        return undefined;
+    }
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+   users: usersReducer,
+});
+
+const persistedState = loadFromLocalStorage();
+
+const store = createStore(
+    rootReducer,
+    persistedState,
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+store.subscribe(() => {
+    saveToLocalStorage({
+        users: store.getState().users
+    });
+});
 
 const app = (
   <Provider store={store}>
