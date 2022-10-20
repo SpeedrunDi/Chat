e8cb2df39b4bfc9306cc4434d856587d33e6eb98
Ilim Alybekov <ilimalybekov2204@gmail.com>
2022-10-20 14:04:15 +0600

    CP-12 Added validation for message

--- api/models/Message.js
+++ api/models/Message.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const validateMessage = async value => {
+  const pattern = /^[ \s\t\n]+/;
+
+  if (pattern.test(value)) return value.replace(pattern, '');
+};
 
 const MessageSchema = new Schema({
   text: {
     type: String,
     required: true,
+    validate: [
+      {validator: validateMessage}
+    ]
   },
   user: {
     type: Schema.Types.ObjectId,
